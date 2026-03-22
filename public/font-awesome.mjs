7ce25116292eaa602f829bfebbe38963a3a10452
rdevelabs <rdevelabs@localhost.localdomain>
2026-03-22 21:26:34 +0700

    initial commit

---
+++ public/font-awesome.mjs
@@ -0,0 +1,6 @@
+import { layer, icon, counter } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
+
+console.log(icon(faEnvelopeSquare));
+
+document.getElementById('camera-icon').innerHTML = icon(faEnvelopeSquare).html[0];

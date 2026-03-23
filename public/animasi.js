fb66e31b37f56749e460fd859206a2b47d029f9e
rdevelabs <rdevelabs@localhost.localdomain>
2026-03-23 19:58:45 +0700

    adding simple animation

---
+++ public/animasi.js
@@ -0,0 +1,41 @@
+import { animate, spring, stagger, splitText } from 'animejs';
+
+const { chars:judul } = splitText('h1', {
+  chars: true,
+})
+const { chars:matakuliah } = splitText('.matakuliah', {
+  chars: true,
+})
+
+animate(judul, {
+  y: [
+    { to: ['-100%', '0%'] },
+  ],
+  opacity: [0,1],
+  duration: 500,
+  ease: spring({bounce: .5, duration: 500}),
+  delay: stagger(50),
+});
+
+animate(matakuliah,{
+    x: [{ to: ['-100', '0%']}],
+    duration: 500,
+    opacity: [0,1],
+    ease: spring({bounce: .5, duration: 500}),
+    delay: stagger(50)
+})
+
+animate('div label', {
+  translateX: [100, 0],
+  duration: 2000,
+  opacity: [0, 1],
+  ease: spring(),
+  delay: stagger(100)
+});
+
+animate('div input, select', {
+  duration: 2000,
+  opacity: [0, 1],
+  ease: spring(),
+  delay: stagger(100)
+});
